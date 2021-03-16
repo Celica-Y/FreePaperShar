4cdf87a250e8afd6852c733b537af93874aaa59e
Cellica_Y <86star.train@gmail.com>
2021-03-16 14:12:49 +0900

    first commit

---
+++ src/main/java/com/example/demo/FreePaperApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreePaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreePaperApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/static/js/crop.js
@@ -0,0 +1,74 @@
+
+$(function() {
+  var $preview = $('.crop');
+  
+  // 画像の読み込みと切り抜きへの表示
+      
+      function readFile(input) {
+        if (input.files && input.files[0]) {
+          var reader = new FileReader();
+          
+          reader.onload = function (e) {
+            $('#upload').addClass('ready');
+            $uploadCrop.croppie('bind', {
+              url: e.target.result
+            }).then(function(){
+              console.log('jQuery bind complete');
+            });
+            
+          }
+          
+          reader.readAsDataURL(input.files[0]);
+        }
+      }
+      
+      //  croppieの表示
+      var $uploadCrop = $preview.croppie({
+        viewport: {
+          width: 200,
+          height: 200,
+          type: 'circle'
+        },
+        boundary: {
+          width: 300,
+          height: 300
+        },
+        enableExif: true
+      });
+      
+      
+      //croppieのポップアップ 
+      $('#upload').click(function(e) {
+        $('#cropImagePop').fadeIn();
+    });
+    
+    // キャンセル時
+    $('.modal-btn-cancel').click(function(e) {
+      $('#cropImagePop').hide();
+  });
+
+  
+  // プレビューの表示（決定ボタンで確定）
+    $('#upload').on('change', function () { readFile(this); });
+      $('.upload-result').on('click', function (ev) {
+        $uploadCrop.croppie('result', {
+          type: 'canvas',
+          size: 'viewport'
+        }).then(function (resp) {
+          $("#upload-view").attr('src', resp);
+          $("#cropImagePop").modal('hide');
+          
+        });
+      });
+
+      $('.upload-result').click(function(e) {
+        $('#cropImagePop').hide();
+    });
+  
+
+  //保存はAjaxに。
+
+
+    
+    
+      })
