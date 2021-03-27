2dc4828a2175fe9a5b5eb207cfe59116b55b7266
Cellica_Y <86star.train@gmail.com>
2021-03-27 21:47:53 +0900

    投稿詳細星以外の見た目完了

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -8,38 +8,46 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class controller {
 
-	// indexページの表示
+// indexページの表示
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView indexGet(ModelAndView mv) {
 		mv.setViewName("index"); 
 		return mv;
 	}
 
-	// entryページの表示
+// entryページの表示
 	@RequestMapping(value="/entry", method=RequestMethod.GET)
 	public ModelAndView entryGet(ModelAndView mv) {
 		mv.setViewName("entry"); 
 		return mv;
 	}
 
-	// ログインページの表示
+// ログインページの表示
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public ModelAndView loginGet(ModelAndView mv) {
 		mv.setViewName("login"); 
 		return mv;
 	}
 
-	// 検索ページの表示
+// 検索ページの表示
 	@RequestMapping(value="/search", method=RequestMethod.GET)
 	public ModelAndView referenceGet(ModelAndView mv) {
 		mv.setViewName("search"); 
 		return mv;
 	}
 
-	// 投稿ページの表示(urlはログイン機能追加後に変更)
+// 投稿ページの表示(urlはログイン機能追加後に変更)
 	@RequestMapping(value="/regist", method=RequestMethod.GET)
 	public ModelAndView registerGet(ModelAndView mv) {
 		mv.setViewName("register"); 
 		return mv;
 	}
+
+// 投稿詳細ぺーじの表示
+	@RequestMapping(value="/detail", method=RequestMethod.GET)
+	public ModelAndView datailsGet(ModelAndView mv) {
+	mv.setViewName("details"); 
+	return mv;
+}
+
 }
