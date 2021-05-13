28d22eb3863853735c7f2371f73ad31f817490c4
Cellica_Y <86star.train@gmail.com>
2021-05-13 17:23:35 +0900

    検索機能のみ完成

--- src/main/java/com/example/controller/searchController.java
+++ src/main/java/com/example/controller/searchController.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -63,78 +64,74 @@ public class searchController {
     @RequestMapping(value="/search", method=RequestMethod.POST)
 	public ModelAndView search(
         ModelAndView mv,
-        @RequestParam(name="paperName") String paperName,
-		@RequestParam(name="prefectures") String prefectures,
-		@RequestParam(name="category") String categoryName){
-		 	mv.setViewName("search"); 
-			 List<PaperTable> list = new ArrayList<PaperTable>();
-			
+		@RequestParam(name = "prefectures", required = false) String prefectures,
+		@RequestParam(name ="category", required = false) String categoryName,
+		@RequestParam(name ="paperName", required = false) String paperName
+		){
+	
+			mv.setViewName("search"); 
+			List<PaperTable> list = new ArrayList<PaperTable>();
 
-			// if (prefectures != null) { //都道府県
-			// 	// list = pRepository.findByPrefectures(prefectures);
-				
-			// 	if (category != null) {//都道府県とカテゴリー
-			// 		list = pRepository.findByPaperNameAndCategory(prefectures,category);
-			// 		mv.addObject("size",list.size());
-			// 		mv.addObject("prefectures",prefectures);
-			// 		mv.addObject("category",category);
-			// 	}else 
-				// if(paperName != null) { //名前入力・曖昧検索
-				// 	list  = pRepository.findByPaperName(paperName);
-				// 	mv.addObject("size",list.size());
-				// 	mv.addObject("prefectures",prefectures);
-				// 	mv.addObject("paperName",paperName);
-				// }
+			if(prefectures!=""){
+				if(categoryName!=""){//都道府県とカテゴリ
+					list = pRepository.findByPrefecturesAndCategory(prefectures, categoryName);
+					mv.addObject("size", list.size());
+					mv.addObject("prefectures", prefectures);
+					mv.addObject("categoryName", categoryName);
+					System.out.println("都道府県とカテゴリ");
 
-				if(prefectures != null) {
-					if(categoryName != null){//都道府県とカテゴリー/一致
-						list  = pRepository.findByPrefecturesAndCategory(prefectures, categoryName);
-						mv.addObject("size",list.size());
-						mv.addObject("prefectures",prefectures);
-						mv.addObject("category",categoryName);
-					}else if(paperName != null){//都道府県とペーパー名/一致・曖昧検索
-						list  = pRepository.findByPrefecturesAndPaperName(prefectures, paperName);
-						mv.addObject("size",list.size());
-						mv.addObject("prefectures",prefectures);
-						mv.addObject("paperName",paperName);
-					}
-				// }else{//都道府県のみ/一致
-				// 	list  = pRepository.findByPrefectures(prefectures);
-				// 	mv.addObject("size",list.size());
-				// 	mv.addObject("prefectures",prefectures);
-				// 	mv.addObject("PaperName",PaperName);
+				}else if(paperName!=""){//都道府県とフリペ名
+					list = pRepository.findByPrefecturesAndPaperNameContaining(prefectures, paperName);
+					mv.addObject("size", list.size());
+					mv.addObject("prefectures", prefectures);
+					mv.addObject("paperName", paperName);
+					System.out.println("都道府県とフリペ名");
+
+				}else{//都道府県のみ
+					list = pRepository.findByPrefectures(prefectures);
+					mv.addObject("size", list.size());
+					mv.addObject("prefectures", prefectures);
+					System.out.println("都道府県");
 				}
+			}else if(categoryName!=""){//カテゴリのみ
+				if(paperName!=""){
+					list = pRepository.findByCategoryAndPaperNameContaining(categoryName, paperName);
+					mv.addObject("size", list.size());
+					mv.addObject("categoryName", categoryName);
+					mv.addObject("paperName", paperName);
+					System.out.println("カテゴリとフリペ");
+				}else{
+					list = pRepository.findByCategory(categoryName);
+					mv.addObject("size", list.size());
+					mv.addObject("categoryName", categoryName);
+					System.out.println("カテゴリのみ");
+				}
+			}else if(paperName!=""){//フリペ名のみ
+					list = pRepository.findByPaperNameContaining(paperName);
+					mv.addObject("size", list.size());
+					mv.addObject("paperName", paperName);
+					System.out.println("フリペのみ");
+			}else{//未入力
+				list = pRepository.findAll();
+				mv.addObject("size", list.size());
+				mv.addObject("list", list);
+				System.out.println("未入力");
+			}
 
-				// if(categoryName != null) { //カテゴリ・一致
-				// 	list  = pRepository.findByCategory(categoryName);
-				// 	mv.addObject("size",list.size());
-				// 	mv.addObject("category",categoryName);
-				// 	mv.addObject("PaperName",PaperName);
-				// }
-			// 	else {		// エリアだけ入力
-			// 		list = pRepository.findByPrefectures(prefectures);
-			// 		mv.addObject("size",list.size());
-			// 		mv.addObject("prefectures",prefectures);
-			// 	}
+			if(prefectures!="" && categoryName!="" && paperName!=""){
+				list = pRepository.findByPrefecturesAndCategoryAndPaperName(prefectures, categoryName, paperName);
+				mv.addObject("size", list.size());
+				mv.addObject("prefectures", prefectures);
+				mv.addObject("categoryName", categoryName);
+				mv.addObject("paperName", paperName);
+				System.out.println("全一致");
+			}
 
-			// }else if(PaperName != null && category == null) {
-			// 		list = pRepository.findByPaperName(PaperName);
-			// 		mv.addObject("size",list.size());
-			// 		mv.addObject("PaperName",PaperName);
-			// }else if(category != null) {
-			// 		list = pRepository.findByCategory(category);	
-			// 		mv.addObject("size",list.size());		
-			// 		mv.addObject("category",category);	
-			// }else{
-			// 		list = pRepository.findAll();
-			// 		mv.addObject("size",list.size());
-			// }
-			// if(list.size() == 0) {
-			// 	// mv.addObject("msg","該当データがありません。");
-			// }
+			if(list.size()==0){
+				mv.addObject("msg", "該当データがありません。");
+			}
 			mv.addObject("list", list);
-			// return new ModelAndView("redirect:/search");
-				return mv;
+			return mv;
         /**
          * 画像データはbase64にエンコードしてhtmlに渡す
          * th:src="${base64data}"
--- src/main/java/com/example/repository/peperSearchRepository.java
+++ src/main/java/com/example/repository/peperSearchRepository.java
@@ -1,6 +1,9 @@
 package com.example.repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import com.example.entity.PaperTable;
 
@@ -10,10 +13,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface peperSearchRepository extends JpaRepository<PaperTable, Long>{
     public List<PaperTable> findByPaperNameContaining(String paperName);
-    public List<PaperTable> findByPaperNameAndCategory(String PaperName, String category);
+    public List<PaperTable> findByPaperNameAndCategory(String paperName, String category);
     public List<PaperTable> findByPrefectures(String prefectures);
     public List<PaperTable> findByPaperName(String paperName);
     public List<PaperTable> findByCategory(String category);
-    public List<PaperTable> findByPrefecturesAndPaperName(String prefectures, String paperName);
+    public List<PaperTable> findByPrefecturesAndPaperNameContaining(String prefectures, String paperName);
     public List<PaperTable> findByPrefecturesAndCategory(String prefectures, String category);
+    public List<PaperTable> findByCategoryAndPaperNameContaining(String category, String paperName);
+    public List<PaperTable> findByPrefecturesAndCategoryAndPaperName(String prefectures, String category, String paperName);
 }
